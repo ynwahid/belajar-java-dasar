8a92af3cbc6e733f62dcf76354985658479116d0
ynw99 <yusufedu99@gmail.com>
2021-11-07 22:12:12 +0700

    "variable"

---
+++ src/Variable.java
@@ -0,0 +1,25 @@
+public class Variable {
+
+  public static void main(String[] args) {
+
+    String name;
+    name = "Yusuf Nur Wahid";
+
+    System.out.println(name);
+
+    int age = 30;
+    String address = "Indonesia";
+
+    System.out.println(age);
+    System.out.println(address);
+
+    name = "Cakcup";
+    System.out.println(name);
+
+    var firstName = "Yusuf";
+    var middleName = "Nur";
+    var lastName = "Wahid";
+
+    final var application = "Belajar Java";
+  }
+}
