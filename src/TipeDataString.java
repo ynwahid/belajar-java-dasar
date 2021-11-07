57336b737d23d1a592800fe80f58819474e2fe58
ynw99 <yusufedu99@gmail.com>
2021-11-07 19:46:52 +0700

    tipe data string

---
+++ src/TipeDataString.java
@@ -0,0 +1,14 @@
+public class TipeDataString {
+
+  public static void main(String[] args) {
+
+    String firstName = "Yusuf";
+    String lastName = "Nur Wahid";
+    String fullName = firstName + " " + lastName;
+
+    System.out.println(firstName);
+    System.out.println(lastName);
+
+    System.out.println(fullName);
+  }
+}
