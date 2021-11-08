c34b5c0a13506e9956616e646a2353041f0a1c3e
ynw99 <yusufedu99@gmail.com>
2021-11-08 21:45:04 +0700

    comment

---
+++ src/Comment.java
@@ -0,0 +1,19 @@
+public class Comment {
+
+  public static void main(String[] args) {
+
+    sum(10, 10);
+  }
+
+  /**
+   * Method untuk menjumlahkan value1 dan value2
+   *
+   * @param value1 bilangan pertama
+   * @param value2 bilangan kedua
+   * @return hasil penjumlahan value1 dan value2
+   */
+  static int sum(int value1, int value2) {
+    // jumlah value1 + value2
+    return value1 + value2;
+  }
+}
